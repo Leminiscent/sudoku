107b8412e198212feb82f3f137abc89177c2023c
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-02-25 16:13:15 -0500

    Add backtracking solver to Board class

--- sudoku.py
+++ sudoku.py
@@ -66,3 +66,6 @@ class Board:
             for guess in range(1, 10):
                 if self.is_valid(next_empty, guess):
                     row, col = next_empty
+                    self.board[row][col] = guess
+                    if self.solver():
+                        return True
