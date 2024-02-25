69378b147526b84a5ffb58fc126e9455be46a6db
Lemuel Nogueira <94577645+Leminiscent@users.noreply.github.com>
2024-02-25 16:33:41 -0500

    Add puzzle solver and print solution if solvable

--- sudoku.py
+++ sudoku.py
@@ -75,3 +75,8 @@ class Board:
     def solve_sudoku(board):
         gameboard = Board(board)
         print(f"\nPuzzle to solve:\n{gameboard}")
+        if gameboard.solver():
+            print("\nSolved puzzle:")
+            print(gameboard)
+        else:
+            print("\nThe provided puzzle is unsolvable.")
